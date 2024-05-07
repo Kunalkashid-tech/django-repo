134f280a6195726b1199578e61e68b0eec07cf7c
kingkunal27 <kunalkashid2002@gmail.com>
2024-05-07 23:13:55 +0530

    changes done

--- hello_project/hello_project/settings.py
+++ hello_project/hello_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'HelloWorld_App',
+    'HelloWorld_App.apps.HelloworldAppConfig',
 ]
 
 MIDDLEWARE = [
